1ad428094ca3ae9713642991e1a8a11f919dd990
widici <84205124+widici@users.noreply.github.com>
2025-05-23 14:25:42 +0200

    Fixed issues w/ LDR:s & other misc. changes

--- src/main.cpp
+++ src/main.cpp
@@ -2,38 +2,56 @@
 #include <Servo.h>
 
 const int N_SERVOS = 3;
-const int POT_PINS[N_SERVOS] = {0, 1, 2};
+const int POT_PINS[N_SERVOS] = {A0, A1, A2};
 const int SERVO_PINS[N_SERVOS] = {6, 5, 3};
 Servo servos[N_SERVOS];
 
-const int WIN_PIN = 12;
-const int FINISH_PIN = 13;
+const int LIGHT_THRESHOLD = 150;
+const int WIN_PIN = A3;
+const int FINISH_PIN = A4;
 bool has_won = false;
+
+const int IN_PIN = 12;
+const int OUT_PIN = 13;
 
 void setup() {
     Serial.begin(9600);
     for (int i = 0; i < N_SERVOS; ++i) {
         servos[i].attach(SERVO_PINS[i]);
     }
+    reset();
     pinMode(WIN_PIN, INPUT);
     pinMode(FINISH_PIN, INPUT);
 }
 
 void loop() {
+    while (digitalRead(12) == LOW) {
+        delay(10);
+    }
     for (int i = 0; i < N_SERVOS; i++) {
         int val = analogRead(POT_PINS[i]);
         val = map(val, 0, 1023, 0, 180);
         servos[i].write(val);
     }
-    if (digitalRead(WIN_PIN) == HIGH) {
+    if (analogRead(WIN_PIN) <= LIGHT_THRESHOLD) {
         has_won = true;
     }
-    if (digitalRead(FINISH_PIN) == HIGH) {
+    if (analogRead(FINISH_PIN) <= LIGHT_THRESHOLD) {
         if (has_won) {
             Serial.println("WON");
-            exit(0);
+            digitalWrite(OUT_PIN, HIGH);
+            delay(3000);  // Sleeps for 3 secs
+            digitalWrite(OUT_PIN, LOW);
+            has_won = false;
         }
-        Serial.println("RESTART");
+        Serial.println("RESET");
+        reset();
     }
     delay(15);
+}
+
+void reset() {
+    for (int i = 0; i < N_SERVOS; i++) {
+        servos[i].write(90);
+    }
 }
